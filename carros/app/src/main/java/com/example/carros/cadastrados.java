165ac0716cf9b4f6df145009489b2d6c9cb92160
ricardoszpo <ricardoszpoganicznetto@gmail.com>
2023-03-27 21:47:37 -0300

    Atualização dia 27/03

--- carros/app/src/main/java/com/example/carros/cadastrados.java
+++ carros/app/src/main/java/com/example/carros/cadastrados.java
@@ -1,15 +1,54 @@
 package com.example.carros;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager2.widget.ViewPager2;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
-public class cadastrados extends AppCompatActivity {
+import com.google.android.material.tabs.TabLayout;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cadastrados);
-        getSupportActionBar().hide();
+public class cadastrados extends AppCompatActivity {
+        TabLayout tabl;
+        ViewPager2 pager;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_cadastrados);
+            getSupportActionBar().hide();
+            tabl = findViewById(R.id.tabl);
+            pager = findViewById(R.id.pager);
+            FragmentManager fm = getSupportFragmentManager();
+            fragmentadapter adaptadorDemonstrativo = new fragmentadapter(fm, getLifecycle());
+            pager.setAdapter(adaptadorDemonstrativo);
+            tabl.setSelectedTabIndicatorColor(Color.parseColor("#000000"));
+            tabl.setTabTextColors(Color.parseColor("#000000"), Color.parseColor("#FF0000"));
+            tabl.addTab(tabl.newTab().setText("Médias"));
+            tabl.addTab(tabl.newTab().setText("Todos"));
+            tabl.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+                @Override
+                public void onTabSelected(TabLayout.Tab tab) {
+                    pager.setCurrentItem(tab.getPosition());
+                }
+
+                @Override
+                public void onTabUnselected(TabLayout.Tab tab) {
+
+                }
+
+                @Override
+                public void onTabReselected(TabLayout.Tab tab) {
+
+                }
+            });
+            pager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+                @Override
+                public void onPageSelected(int position) {
+                    super.onPageSelected(position);
+                    tabl.selectTab(tabl.getTabAt(position));
+                }
+            });
+        }
     }
-}
